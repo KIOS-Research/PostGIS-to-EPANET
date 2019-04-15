317f278a62944b7909191fa2cb9cad6a205352e1
mariosmsk <mariosmsk@gmail.com>
2019-04-15 15:13:03 +0300

    Update PostGIStoEPANET.py

--- PostGIStoEPANET.py
+++ PostGIStoEPANET.py
@@ -1,3 +1,20 @@
+# !/usr/bin/python
+"""
+/***************************************************************************
+copyright            : (C) 2019 by Marios S. Kyriakou, University of Cyprus,
+                       KIOS Research and Innovation Center of Excellence (KIOS CoE)
+email                : mariosmsk@gmail.com
+ ***************************************************************************/
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+"""
+
 import os
 import psycopg2
 import re
@@ -35,7 +52,7 @@ pumps_shp = cfg['pumps_shp']
 valves_shp = cfg['valves_shp']
 
 if dma not in dmalist:
-    print 'Wrong DMA.'
+    print ('Wrong DMA.')
     sys.exit(0)
 
 NN="dbname=" + DMA + " host=" + host + " user=" + username + " password=" + password + " port=" + port
